23c5b2a03f5ce8a9575efdfa0ebbf0fde810cbdc
Pc <Pc@DESKTOP-765R19V>
2018-09-08 15:51:47 +0600

    add users table firstname, Isactive, registered property

---
+++ MembershipApps/Migrations/201809080950530_AddUsersTableProperty.cs
@@ -0,0 +1,22 @@
+namespace MembershipApps.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddUsersTableProperty : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "FirstName", c => c.String());
+            AddColumn("dbo.AspNetUsers", "IsActive", c => c.Boolean(nullable: false));
+            AddColumn("dbo.AspNetUsers", "Registered", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "Registered");
+            DropColumn("dbo.AspNetUsers", "IsActive");
+            DropColumn("dbo.AspNetUsers", "FirstName");
+        }
+    }
+}
