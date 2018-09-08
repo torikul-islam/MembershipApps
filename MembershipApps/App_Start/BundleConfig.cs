f0f925f20011e3d81978052cf2a00d54cfdb1ea3
Pc <Pc@DESKTOP-765R19V>
2018-09-08 14:30:43 +0600

    InitialSetupAdminMenu

--- MembershipApps/App_Start/BundleConfig.cs
+++ MembershipApps/App_Start/BundleConfig.cs
@@ -25,6 +25,9 @@ namespace MembershipApps
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/Admin").Include(
+                "~/Scripts/AdminMenu.js"));
         }
     }
 }
