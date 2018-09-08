bcbeeeea63bccfc6cc5f27dc8f69113c114cec7d
Pc <Pc@DESKTOP-765R19V>
2018-09-08 15:25:29 +0600

    add Item and item table to database

--- MembershipApps/Models/IdentityModels.cs
+++ MembershipApps/Models/IdentityModels.cs
@@ -33,6 +33,8 @@ namespace MembershipApps.Models
 
         public DbSet<Section> Sections { get; set; }
         public DbSet<Part> Parts { get; set; }
+        public DbSet<ItemType> ItemTypes { get; set; }
+        public DbSet<Item> Items { get; set; }
 
     }
 }
